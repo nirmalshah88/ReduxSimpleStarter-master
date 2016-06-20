626f009200aae7e6f36f641e92ecbfd48ffa1a80
Nirmal Shah <nirmalshah2000@gmail.com>
2016-06-20 12:46:28 -0700

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+// Create a new component. This component should produce some HTML
+const App = function() {
+  return <div>Hi!</div>;
+}
 
-import App from './components/app';
-import reducers from './reducers';
+// Take this component's generated HTML & put it on the page (in the DOM)
+React.render(App);
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// NOTES:
+// const = constant (opp. of var) (ES6)
+// webpack + babel converts JSX (ES6) to vanilla JS
+// JSX = JS code that can produce HTML
